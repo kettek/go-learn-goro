f3b79446baabe78b3b91a833df3c580b92c756a5
kts of kettek (nyaa) <kettek1@kettek.net>
2019-06-28 16:33:18 -0700

    Finish 3:6 and 3:7!

---
+++ curriculum/part-3/6.go
@@ -0,0 +1,102 @@
+package mapping
+
+// GameMap is our map data type.
+type GameMap struct {
+	Width, Height int
+	Tiles         [][]Tile
+}
+
+// Initialize initializes a GameMap's Tiles to match its Width and Height. It also sets up some coordinates to block movement and sight.
+func (g *GameMap) Initialize() {
+	g.Tiles = make([][]Tile, g.Width)
+
+	for x := range g.Tiles {
+		g.Tiles[x] = make([]Tile, g.Height)
+		for y := range g.Tiles[x] {
+			g.Tiles[x][y] = Tile{
+				BlockSight:    true,
+				BlockMovement: true,
+			}
+		}
+	}
+}
+
+// MakeMap creates a new randomized map. This is built according to the passed arguments.
+func (g *GameMap) MakeMap(maxRooms, roomMinSize, roomMaxSize int, player *entity.Entity) {
+	var rooms []*Rect
+
+	for r := 0; r < maxRooms; r++ {
+		// Generate a random width and height.
+		width := roomMinSize + goro.Random.Intn(roomMaxSize)
+		height := roomMinSize + goro.Random.Intn(roomMaxSize)
+		// Generate a random position within the map boundaries.
+		x := goro.Random.Intn(g.Width - width - 1)
+		y := goro.Random.Intn(g.Height - height - 1)
+		// Create a Rect according to our generated sizes.
+		room := NewRect(x, y, width, height)
+
+		// Iterate through our existing rooms to check for intersection with our new room.
+		intersects := false
+		for _, otherRoom := range rooms {
+			if room.Intersect(otherRoom) {
+				intersects = true
+				break
+			}
+		}
+		// Add the room if there is no intersection found.
+		if !intersects {
+			g.CreateRoom(room)
+			roomCenterX, roomCenterY := room.Center()
+
+			// Always place the player in the center of the first room.
+			if len(rooms) == 0 {
+				player.X = roomCenterX
+				player.Y = roomCenterY
+			} else {
+				prevCenterX, prevCenterY := rooms[len(rooms)-1].Center()
+
+				// Flip a coin if we should tunnel horizontally or vertically first.
+				if goro.Random.Intn(1) == 1 {
+					g.CreateHTunnel(prevCenterX, roomCenterX, prevCenterY)
+					g.CreateVTunnel(prevCenterY, roomCenterY, roomCenterX)
+				} else {
+					g.CreateVTunnel(prevCenterY, roomCenterY, prevCenterX)
+					g.CreateHTunnel(prevCenterX, roomCenterX, roomCenterY)
+				}
+			}
+			// Append our new room to our rooms list.
+			rooms = append(rooms, room)
+		}
+	}
+}
+
+// CreateRoom creates a room from a provided rect.
+func (g *GameMap) CreateRoom(r *Rect) {
+	for x := r.X1 + 1; x < r.X2; x++ {
+		for y := r.Y1 + 1; y < r.Y2; y++ {
+			g.Tiles[x][y] = Tile{}
+		}
+	}
+}
+
+// CreateHTunnel creates a horizontal tunnel from x1 to/from x1 starting at y.
+func (g *GameMap) CreateHTunnel(x1, x2, y int) {
+	for x := goro.MinInt(x1, x2); x <= goro.MaxInt(x1, x2); x++ {
+		g.Tiles[x][y] = Tile{}
+	}
+}
+
+// CreateVTunnel creates a vertical tunnel from y1 to/from y2 starting at x.
+func (g *GameMap) CreateVTunnel(y1, y2, x int) {
+	for y := goro.MinInt(y1, y2); y <= goro.MaxInt(y1, y2); y++ {
+		g.Tiles[x][y] = Tile{}
+	}
+}
+
+// IsBlocked returns if the given coordinates are blocking movement.
+func (g *GameMap) IsBlocked(x, y int) bool {
+	// Always block if outside our GameMap's bounds.
+	if x < 0 || x >= g.Width || y < 0 || y >= g.Height {
+		return true
+	}
+}
---
+++ curriculum/part-3/7.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"github.com/kettek/goro"
+	"log"
+
+	"myproject/entity"
+	"myproject/mapping"
+)
+
+func main() {
+	// Initialize goro!
+	if err := goro.InitTCell(); err != nil {
+		log.Fatal(err)
+	}
+
+	goro.Run(func(screen *goro.Screen) {
+		// Screen configuration.
+		screen.SetTitle("My Roguelike")
+
+		// Randomize our seed so the map is randomized per run.
+		goro.SetSeed(goro.RandomSeed())
+
+		// Our initial variables.
+		mapWidth, mapHeight := 80, 24
+		maxRooms, roomMinSize, roomMaxSize := 30, 6, 10
+
+		colors := map[string]goro.Color{
+			"darkWall":   goro.Color{R: 0, G: 0, B: 100, A: 255},
+			"darkGround": goro.Color{R: 50, G: 50, B: 150, A: 255},
+		}
+
+		gameMap := mapping.GameMap{
+			Width:  mapWidth,
+			Height: mapHeight,
+		}
+
+		gameMap.Initialize()
+
+		player := entity.NewEntity(screen.Columns/2, screen.Rows/2, '@', goro.Style{Foreground: goro.ColorWhite})
+		npc := entity.NewEntity(screen.Columns/2-5, screen.Rows/2, '@', goro.Style{Foreground: goro.ColorYellow})
+
+		entities := []*entity.Entity{
+			player,
+			npc,
+		}
+
+		gameMap.MakeMap(maxRooms, roomMinSize, roomMaxSize, player)
+
+		for {
+			// Draw screen.
+			DrawAll(screen, entities, gameMap, colors)
+			ClearAll(screen, entities)
+
+			// Handle events.
+			switch event := screen.WaitEvent().(type) {
+			case goro.EventKey:
+				switch action := handleKeyEvent(event).(type) {
+				case ActionMove:
+					if !gameMap.IsBlocked(player.X+action.X, player.Y+action.Y) {
+						player.Move(action.X, action.Y)
+					}
+				case ActionQuit:
+					goro.Quit()
+				}
+			case goro.EventQuit:
+				return
+			}
+		}
+	})
+}
